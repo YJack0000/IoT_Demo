cc6636370235534fa5479bae7baa7c3b48af07bc
Mark Lai <laimark900317@gmail.com>
2021-05-26 14:36:39 +0800

    整理資料夾，新增新增tab-demo

---
+++ Mark_dir/vue-tab-demo/src/store/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import navTab from './modules/navTab'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  modules: {
+    navTab
+  }
+})
+
+export default store
